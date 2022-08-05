2e4584806570fc4d4a3f5239ebcafd439ecc904d
liaozhongxun <869664233@qq.com>
2022-08-05 17:11:18 +0800

    2022-08-05 17:11提交上传

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -1,11 +1,52 @@
 var API = require('../../utils/api.js')
 Page({
     data: {
-        swiperArr: [1,2,3]
+        swiperArr: [1, 2, 3],
+        activecode: -1,
+        show: false,
+        menuList: [{
+                code: 1,
+                name: "经纬度"
+            },
+            {
+                code: 2,
+                name: "1"
+            },
+            {
+                code: 3,
+                name: "1"
+            },
+            {
+                code: 4,
+                name: "1"
+            },
+            {
+                code: 5,
+                name: "1"
+            },
+            {
+                code: 6,
+                name: "1"
+            },
+            {
+                code: 7,
+                name: "1"
+            },
+            {
+                code: 8,
+                name: "1"
+            },
+            {
+                code: 9,
+                name: "1"
+            },
+        ],
     },
     onLoad: function () {
         var that = this
-        API.ajax({count:3}, function (res) {
+        API.ajax({
+            count: 3
+        }, function (res) {
             console.log(res)
             that.setData({
                 swiperArr: res.data
@@ -13,5 +54,33 @@ Page({
         });
 
         console.log(this.data.swiperArr)
-    }
+    },
+    showPopup() {
+        this.setData({
+            show: true
+        });
+    },
+
+    onClose() {
+        this.setData({
+            show: false
+        });
+    },
+    getLonLat() {
+        console.log('fdsafafdsa')
+    },
+    execCmd(event) {
+        this.setData({
+            activecode: event.currentTarget.dataset.code,
+            show:true
+        })
+        switch (event.currentTarget.dataset.code) {
+            case 1:
+                this.getLonLat()
+                break;
+            default:
+                console.log(event.currentTarget.dataset)
+                break;
+        }
+    },
 })
