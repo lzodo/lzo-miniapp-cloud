6b17e60090374c6c14762ebd02cd4efc8b3419b0
liaozhongxun <869664233@qq.com>
2022-08-16 17:38:37 +0800

    2022-08-16 17:38提交上传

--- cpmponents/Lonlat/Lonlat.js
+++ cpmponents/Lonlat/Lonlat.js
@@ -18,6 +18,32 @@ Component({
      * 组件的方法列表
      */
     methods: {
-
+        init(){
+            console.log(wx)
+            wx.makePhoneCall({
+                phoneNumber: '18060849356', //仅为示例，并非真实的电话号码
+                success:(res)=>{
+                    console.log(res)
+                },
+                fail:(err)=>{
+                    console.log(err)
+                }
+            })
+        }
+    },
+    lifetimes: {
+        // 组件的生命周期函数，用于声明组件的生命周期
+        attached: function () {
+            console.log(1)
+        },
+        ready: function () {
+            this.init()
+        },
+        moved: function () {
+            console.log(3)
+        },
+        detached: function () {
+            console.log(4)
+        },
     }
 })
